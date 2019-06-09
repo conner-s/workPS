ba1f2c8c62a784d639e24c55ecfa8d59a11e57f4
Conner <32659799+conner-s@users.noreply.github.com>
2019-06-09 15:00:37 -0400

    Update mylghealth-reso.ps1
    Day2 of work, added further GUI

--- mylghealth-reso.ps1
+++ mylghealth-reso.ps1
@@ -1,7 +1,7 @@
 ﻿$userChoice = 9999
 
 
-
+#Main GUI panel with buttons to autocopy and autoTicket
 function makeGUI{
     #Main Form window initialization
     Add-Type -AssemblyName System.Windows.Forms
@@ -63,30 +63,177 @@ function makeGUI{
     )
 
 
+
+    #Auto MyLGHealth Ticket Button
+    $autolghhealth_Button = New-Object System.Windows.Forms.Button
+    $autolghhealth_Button.Location = New-Object System.Drawing.Size(20, 40)
+    $autolghhealth_Button.Size = New-Object System.Drawing.Size(120,23)
+    $autolghhealth_Button.Text = "Auto MyLGHealth"
+    $main_form.Controls.Add($autolghhealth_Button)
+
+    $autolghhealth_Button.Add_Click({
+        open-MyLGHinput
+        $outLabel.Text = "Opening Text for Ticket"
+        }
+    )
+
+    #VDI Button Declaration
+    $vdi_Button = New-Object System.Windows.Forms.Button
+    $vdi_Button.Location = New-Object System.Drawing.Size(410, 10)
+    $vdi_Button.Size = New-Object System.Drawing.Size(120,23)
+    $vdi_Button.Text = "VDI"
+    $main_form.Controls.Add($vdi_Button)
+
+
+    $vdi_Button.Add_Click({
+        vdi-copy
+        $outLabel.Text = "Set Clipboard to VDI Resolution"
+        }
+    )
+
+
     $main_form.ShowDialog()
 }
 
 
+
+
+
+
+#TextBox form to submit a MyLGHealth Ticket
+function open-MyLGHinput{
+    #MyLGHealth Text Input window initialization
+    Add-Type -AssemblyName System.Windows.Forms
+    $myLGHealthPatient_form = New-Object System.Windows.Forms.Form
+    $myLGHealthPatient_form.Text ='MyLGHealth AutoTicket'
+    $myLGHealthPatient_form.Width = 400
+    $myLGHealthPatient_form.Height = 400
+
+    #Label instructing to Copy-Paste Patient overview info
+    Add-Type -AssemblyName System.Windows.Forms
+    $MyLGHTextBoxLabel = New-Object System.Windows.Forms.Label
+    $MyLGHTextBoxLabel.Text = "Please Copy-Paste the patient's name and PCP"
+    $MyLGHTextBoxLabel.Location = New-Object System.Drawing.Point(20, 5)
+    $MyLGHTextBoxLabel.AutoSize = $true
+    $myLGHealthPatient_form.Controls.Add($MyLGHTextBoxLabel)
+
+
+    #Box to put Patient name and PCP
+    $patTextBox = New-Object System.Windows.Forms.TextBox
+    $patTextBox.Location = New-Object System.Drawing.Point(10,40)
+    $patTextBox.Size = New-Object System.Drawing.Size(350,200)
+    $patTextBox.AutoSize = $true
+    $patTextBox.Multiline = $true
+    $myLGHealthPatient_form.Controls.Add($patTextBox)
+
+
+    #MyLGHealth Submit Button
+    $MyLGHTextSub_Button = New-Object System.Windows.Forms.Button
+    $MyLGHTextSub_Button.Location = New-Object System.Drawing.Size(260, 300)
+    $MyLGHTextSub_Button.Size = New-Object System.Drawing.Size(120,23)
+    $MyLGHTextSub_Button.Text = "Submit"
+    $myLGHealthPatient_form.Controls.Add($MyLGHTextSub_Button)
+
+
+    $MyLGHTextSub_Button.Add_Click({
+        submitMyLGHealthTicketConfirm
+        }
+    )
+
+
+    $myLGHealthPatient_form.ShowDialog()
+
+}
+
+
+#Confirmation Window to submit a ticket
+function submitMyLGHealthTicketConfirm{
+    #MyLGHealth Text Input window initialization
+    Add-Type -AssemblyName System.Windows.Forms
+    $myLGHealthPatientConfirm_form = New-Object System.Windows.Forms.Form
+    $myLGHealthPatientConfirm_form.Text ='Confirm?'
+    $myLGHealthPatientConfirm_form.Width = 300
+    $myLGHealthPatientConfirm_form.Height = 125
+
+    #Confirm?
+    Add-Type -AssemblyName System.Windows.Forms
+    $MyLGHConfirmLabel = New-Object System.Windows.Forms.Label
+    $MyLGHConfirmLabel.Text = "Are you sure you would like to submit this ticket?"
+    $MyLGHConfirmLabel.Location = New-Object System.Drawing.Point(20, 5)
+    $MyLGHConfirmLabel.AutoSize = $true
+    $myLGHealthPatientConfirm_form.Controls.Add($MyLGHConfirmLabel)
+
+
+
+    #Submit button for AutoTicket 
+    $MyLGHSubmit_Button = New-Object System.Windows.Forms.Button
+    $MyLGHSubmit_Button.Location = New-Object System.Drawing.Size(10, 50)
+    $MyLGHSubmit_Button.Size = New-Object System.Drawing.Size(120,23)
+    $MyLGHSubmit_Button.Text = "Submit"
+    $myLGHealthPatientConfirm_form.Controls.Add($MyLGHSubmit_Button)
+
+
+    $MyLGHSubmit_Button.Add_Click({
+        submitMyLGHealthTicket
+        }
+    )
+
+    #Cancel button for AutoTicket
+    $MyLGHCancel_Button = New-Object System.Windows.Forms.Button
+    $MyLGHCancel_Button.Location = New-Object System.Drawing.Size(150, 50)
+    $MyLGHCancel_Button.Size = New-Object System.Drawing.Size(120,23)
+    $MyLGHCancel_Button.Text = "Cancel"
+    $myLGHealthPatientConfirm_form.Controls.Add($MyLGHCancel_Button)
+
+
+    $MyLGHCancel_Button.Add_Click({
+        $myLGHealthPatientConfirm_form.Close()
+        }
+    )
+
+
+
+    $myLGHealthPatientConfirm_form.ShowDialog()
+}
+
+
+
+function submitMyLGHealthTicket{
+    Write-Host $patTextBox.Text
+}
+
+
+
+
 #Function to copy MyLGHealth unlock/password reset resolution
 function mylgh-copy{
-    cd C:\Users\conne\Desktop
+    cd "C:\Users\cs243\Desktop\PS scripts"
     Get-Content mylghealth.txt | Set-Clipboard
     Write-Host "Copied MyLGHealth Resolution`n`n"
 }
 
 #Function to copy Resolution for PDS 
 function pds-copy{
-    cd C:\Users\conne\Desktop
+    cd "C:\Users\cs243\Desktop\PS scripts"
     Get-Content pds.txt | Set-Clipboard
     Write-Host "Copied PDS Resolution`n`n"
 }
 
 #Function to copy Resolution for DUO
 function duo-copy{
-    cd C:\Users\conne\Desktop
+    cd "C:\Users\cs243\Desktop\PS scripts"
     Get-Content duo.txt | Set-Clipboard
     Write-Host "Copied DUO Resolution`n`n"
 }
+
+#Function to copy Resolution for VDI
+function vdi-copy{
+    cd "C:\Users\cs243\Desktop\PS scripts"
+    Get-Content vdi.txt | Set-Clipboard
+    Write-Host "Copied VDI Resolution`n`n"
+}
+
+
 
 #CLI Menu for copying commands
 function cli-copyier{
@@ -98,6 +245,7 @@ Please choose a resolution to copy`n
 1: MyLGHealth`n
 2: PDS`n
 3: DUO`n
+4: VDI`n
 0: Quit`n"
           $userChoice = Read-Host "Please enter your choise: "
           clear
@@ -105,7 +253,13 @@ Please choose a resolution to copy`n
            1 {mylgh-copy}
            2 {pds-copy}
            3 {duo-copy}
+           4 {vdi-copy}
            0 {break}
            }
     }
+}
+
+function reloadAPP{
+    ipmo .\autoTicket.ps1 -Force
+    makeGUI
 }
